f277369ad85142fe896d6d1c19fc57105eeba2f9
romigentile <romigentile@hotmail.com>
2024-07-30 11:36:17 -0300

    check type

--- hello.py
+++ hello.py
@@ -10,3 +10,16 @@ Este en un comentario en multilinea,
 Hola mundo
 '''
 
+# Tipo de datos
+
+print("Hola mundo, hola Python")    # Cadena de txt
+print('Hola mundo, hola Python')    # Cadena de txt
+
+
+# Consultar el tipo de dato: type
+
+print(type("Soy un dato str"))  # Tipo str
+print(type(5))  # Tipo int
+print(type(1.5))  # Tipo float
+print(type(True))  # Tipo bool
+print(type(1+1j))  # Tipo complex
