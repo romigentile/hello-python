f3efb3b4c68036562412d2ad6020bb4babd87eb6
romigentile <romigentile@hotmail.com>
2024-08-01 16:43:56 -0300

    Funciones del sistema

--- 01_variables.py
+++ 01_variables.py
@@ -20,3 +20,23 @@ print(my_string_variable + ", como estas?, " + str(my_int_variable))
 print(my_string_variable, my_int_variable, my_bool_variable)
 
 print(type(print(my_string_variable, my_int_variable, my_bool_variable))) # None Type
+
+# Funciones del sistema
+
+# cuenta cuantos caracteres tiene la variable, contando espacios
+
+print(len(my_string_variable)) 
+
+# variables en una sola linea - no es buena practica
+
+name, surname, alias, age = "Romina", "Gentile", "romigentile", 32 
+
+print("Mi nombre es: ", name, surname, ". Mi alias es: ", alias, ". Mi edad es: ", age)
+
+# Python no es un lenguaje fuertemente tipado.
+
+address: str = "Calle 123" # Es solo para decir que queremos que sea un string, pero no e suna forma de forzar el tipado.
+print(type(address))
+address = 123 # Se cambia a numero
+print(address)
+print(type(address))
