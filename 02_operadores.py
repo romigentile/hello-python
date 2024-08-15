951b360ecc265c82db08f12f8d727729a23b2650
romigentile <romigentile@hotmail.com>
2024-08-15 11:32:19 -0300

    Operadores aritmeticos

--- 02_operadores.py
+++ 02_operadores.py
@@ -9,3 +9,12 @@ print(3**4) #exponente
 print(10//3) #aproxima la division a un numero entero
 
 print("Hola " + "Mundo")
+print("Hola " + str(5)) #str lo transforma en string
+print("Hola " * 5) #se multiplica la palabra
+
+my_float = 2.5 * 2
+print(my_float)
+print("Hola " * int(my_float))
+
+# Operadores Comparativos
+
