fdf5a4ada624ca63b39eb5060880a4eecac989da
romigentile <romigentile@hotmail.com>
2024-08-15 11:22:13 -0300

    Operadores

---
+++ 02_operadores.py
@@ -0,0 +1,11 @@
+# Operadores - Es muy bueno en operadores de calculo
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3%4)
+print(3**4) #exponente
+print(10//3) #aproxima la division a un numero entero
+
+print("Hola " + "Mundo")
