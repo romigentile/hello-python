44ef18677a3d2ccf0bc022b83c9d01d4162b435b
romigentile <romigentile@hotmail.com>
2024-08-20 23:18:04 -0300

    Strings

--- 03_strings.py
+++ 03_strings.py
@@ -33,3 +33,15 @@ print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age)) # {} para
 
 print(f"Mi nombre es {name} {surname} y mi edad es {age}") # inferencia de datos
 
+# Desempaquetado de caracteres
+
+language = "Python"
+a, b, c, d, e, f = language
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
