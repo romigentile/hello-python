0c45dc9ba3f90632b98035b28a9de31f43563d90
romigentile <romigentile@hotmail.com>
2024-08-20 23:01:45 -0300

    Strings

--- 03_strings.py
+++ 03_strings.py
@@ -1,2 +1,24 @@
 ## Strings 
 
+my_string_1 = "Hola mundo"
+print(my_string_1)
+
+my_string_2 = 'Hola mundoo'
+print(my_string_2)
+
+my_string_3 = """Hola mundooo"""
+print(my_string_3)
+
+my_string_4 = '''Hola mundoooo'''
+print(my_string_4)
+
+print(my_string_1 + " " + my_string_2)
+
+my_new_line_string = "Hola mundo\ncon salto de linea"
+print(my_new_line_string)
+
+my_tab_string = "Hola mundo\n\tcon tabulador y salto de linea"
+print(my_tab_string)
+
+my_scape_string = "Hola mundo \\n con barra invertida"
+print(my_scape_string)
