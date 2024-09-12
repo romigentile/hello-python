d0162262e458683aacd44f7badc81ba0f21e351e
romigentile <romigentile@hotmail.com>
2024-09-12 10:43:37 -0300

    Tuplas

--- 05_tuplas.py
+++ 05_tuplas.py
@@ -22,3 +22,18 @@ my_sum_tuple = my_tuple + my_other_tuple
 print(my_sum_tuple)
 
 print(my_sum_tuple[3:6])
+
+
+# Si quiero q la tupla se modifique, tengo que cambiar de tipo, y convertirlo en lista.
+
+my_tuple = list(my_tuple)
+print(type(my_tuple))
+
+my_tuple[0] = 33
+print(my_tuple)
+
+# La reconvierto a tupla
+
+my_tuple = tuple(my_tuple)
+print(my_tuple)
+print(type(my_tuple))
