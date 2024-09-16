faaaad4366d6d05b1d6332d58a566d5dafc69fd5
romigentile <romigentile@hotmail.com>
2024-09-16 13:16:57 -0300

    Sets

--- 06_sets.py
+++ 06_sets.py
@@ -4,5 +4,8 @@ my_set = set()
 
 print(type(my_set))
 
-my_other_set = {1, 2, 3, 4, 5}
-print(type(my_other_set))
+my_other_set_1 = {1, 2, 3, 4, 5}
+print(type(my_other_set_1))
+
+my_other_set_2 = set([1, 2, 3, 4, 5])
+print(type(my_other_set_2))
