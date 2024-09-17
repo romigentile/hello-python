e0c8ee222e373b583d3e028ce3d79b9648333473
romigentile <romigentile@hotmail.com>
2024-09-17 11:47:40 -0300

    Sets

--- 06_sets.py
+++ 06_sets.py
@@ -1,11 +1,14 @@
 ## Sets: es un tipo de dato
 
 my_set = set()
-
 print(type(my_set))
 
-my_other_set_1 = {1, 2, 3, 4, 5}
+my_other_set_1= {} # vacio es un diccionario
+print(type(my_other_set_1))
+
+my_other_set_1 = {"Gentile", "Romi", 32, 1.64}
 print(type(my_other_set_1))
 
 my_other_set_2 = set([1, 2, 3, 4, 5])
 print(type(my_other_set_2))
+
