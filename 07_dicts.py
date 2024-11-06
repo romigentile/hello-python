cbfbcbbbf0eaf4410dd9e6b4789ceb56a32afe9c
romigentile <romigentile@hotmail.com>
2024-11-06 11:42:14 -0300

    dicts

--- 07_dicts.py
+++ 07_dicts.py
@@ -5,3 +5,6 @@ print(type(my_dict))
 
 my_other_dict = {} # tambien es un dict
 print(type(my_other_dict))
+
+my_other_dict = {"Nombre": "Romina", "Apellido": "Gentile", "Edad": 32} # relacion clave-valor
+print(my_other_dict) 
