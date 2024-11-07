a8df78b36a2c38e1d3968de90fcbcd3583355e18
romigentile <romigentile@hotmail.com>
2024-11-07 14:44:00 -0300

    conditionals

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -1,2 +1,12 @@
-## Condicionales
+## Condicionales - Establecen flujos de ejecucion del codigo - si se cumple una condicion ejecuto
+
+my_condition = True
+
+if my_condition: 
+    print("Se ejecuta la condicion del if")
+
+print("La ejecucion continua")
+
+
+
 
