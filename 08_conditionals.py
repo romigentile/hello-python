f4183d4e9062afe3b8e5ff58c3c4c7940111ebb0
romigentile <romigentile@hotmail.com>
2024-11-13 13:51:12 -0300

    Conditionals

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -1,11 +1,18 @@
 ## Condicionales - Establecen flujos de ejecucion del codigo - si se cumple una condicion ejecuto
 
-my_condition = True
+my_condition = False
 
 if my_condition: 
     print("Se ejecuta la condicion del if")
 
+my_condition = 5*2
+
+if my_condition == 9:
+    print("Se ejecuta la condicion del segundo if")
+
 print("La ejecucion continua")
+
+
 
 
 
