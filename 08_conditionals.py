c1db729a5dfb6780564bb53882cb878a4da6b521
romigentile <romigentile@hotmail.com>
2024-11-13 14:01:44 -0300

    Conditionals

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -5,15 +5,15 @@ my_condition = False
 if my_condition: 
     print("Se ejecuta la condicion del if")
 
-my_condition = 5*2
+my_condition = 5*3
 
 if my_condition == 10:
     print("Se ejecuta la condicion del segundo if")
 
-if my_condition > 10:
-    print("Es mayor que 10")
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor a 20")
 else:
-    print("Es menor o igual que 10")
+    print("Es menor o igual que 10 o mayor o igual a 20")
 
 print("La ejecucion continua")
 
