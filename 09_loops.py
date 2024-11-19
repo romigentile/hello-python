0aa90c9a535af34a1b2887c3729fe206309f389d
romigentile <romigentile@hotmail.com>
2024-11-19 16:06:49 -0300

    Loops

--- 09_loops.py
+++ 09_loops.py
@@ -7,5 +7,7 @@ my_condition = 0
 while my_condition < 10:
     print(my_condition)
     my_condition += 1 # si la condicion no cambia es un while infinito
+else: 
+    print("Mi condicion es > = 10") # cuando pasa el 10 se va al else, sale del bucle
 
 print("Sale del bucle")
